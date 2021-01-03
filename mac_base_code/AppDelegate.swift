f0027db7d08db9f4a148600fa00c34d4e05060ac
lcver <crackover1092@gmail.com>
2021-01-03 19:44:20 +0700

    [init] xcodeproj

---
+++ mac_base_code/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mac_base_code
+//
+//  Created by Yapindo on 03/01/21.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mac_base_code/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  mac_base_code
+//
+//  Created by Yapindo on 03/01/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
