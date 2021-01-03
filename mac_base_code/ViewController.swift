8ecd651e407ebc8a631e6a08f1c4eb7390642407
lcver <crackover1092@gmail.com>
2021-01-03 22:22:54 +0700

    [finish] storyboard view controller

--- mac_base_code/ViewController.swift
+++ mac_base_code/ViewController.swift
@@ -8,10 +8,13 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    private var AppName = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        AppName = ["Ilmu Penyakit Dalam"]
         // Do any additional setup after loading the view.
     }
 
@@ -20,7 +23,18 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
+extension ViewController: NSCollectionViewDelegate, NSCollectionViewDataSource {
+    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        let item = collectionView.makeItem(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "AppItem"), for: indexPath)
+        
+        item.imageView?.image = NSImage(named: String(AppName[0]))
+        
+        return item
+    }
+}
