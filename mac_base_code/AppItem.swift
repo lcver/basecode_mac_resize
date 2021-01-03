3fea6c4f69ca68f7887937f50ecf37f7b22cfd5e
lcver <crackover1092@gmail.com>
2021-01-03 22:16:33 +0700

    [add] bahan

---
+++ mac_base_code/AppItem.swift
@@ -0,0 +1,17 @@
+//
+//  AppItem.swift
+//  mac_base_code
+//
+//  Created by Yapindo on 03/01/21.
+//
+
+import Cocoa
+
+class AppItem: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
