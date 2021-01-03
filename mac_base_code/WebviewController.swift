ee473a5d449161a87c43fe43d86d2d7a3b663cc1
lcver <crackover1092@gmail.com>
2021-01-03 22:43:45 +0700

    [add] Webview controller

---
+++ mac_base_code/WebviewController.swift
@@ -0,0 +1,17 @@
+//
+//  WebviewController.swift
+//  mac_base_code
+//
+//  Created by Yapindo on 03/01/21.
+//
+
+import Cocoa
+
+class WebviewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
